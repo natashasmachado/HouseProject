3934e7b6cab6450d45370d9771c43cdc3a563f9a
Natasha Machado <natashasantosmachado@gmail.com>
2023-03-07 18:19:59 -0800

    code

---
+++ House.swift
@@ -0,0 +1,28 @@
+//
+//  House.swift
+//  Assignment1
+//
+//  Created by Derrick Park on 2023-03-03.
+//
+
+import Foundation
+
+class House {
+  var border: Character
+  var fill: Character
+  var size: Int
+  
+  var perimeter: Int {
+    return 0
+  }
+  
+  var area: Double {
+    return 0
+  }
+  
+  func grow() { }
+  
+  func shrink() { }
+  
+  func draw() { }
+}
